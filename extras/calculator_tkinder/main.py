c10648f7493547ca656f7740256d347e4382be65
Dayan Arango <dayaara6@gmail.com>
2022-09-13 20:31:37 -0300

    calculator and pomodoro update

--- extras/calculator_tkinder/main.py
+++ extras/calculator_tkinder/main.py
@@ -1,19 +1,23 @@
 # Import module
 import tkinter
+import math
 # TODO: round
 # TODO: clear
 # Create object
+
+BLUE = "#1A374D"
 window = tkinter.Tk()
+window.config(bg=BLUE)
 # Adjust size
 window.geometry("300x500")
-bg = tkinter.PhotoImage(file="image.png")
-# Show image using label
-label = tkinter.Label(window, image=bg)
-label.place(x=0, y=0)
 
+canvas = tkinter.Canvas(width=300, height=500, bg=BLUE, highlightthickness=0)
+image = tkinter.PhotoImage(file="image.png")
+canvas.create_image(150, 250, image=image)
+canvas.place(x=0, y=0)
 equation = tkinter.StringVar()
 
-expression_field = tkinter.Label(window, width=10, textvariable=equation)
+expression_field = tkinter.Label(window, width=10, textvariable=equation, bg=BLUE)
 
 expression_field.place(x=160, y=140)
 
@@ -31,28 +35,28 @@ def press(num):
             r = expression.find("-")
             l_n1 = float(expression[0:r])
             l_n2 = float(expression[r+1:])
-            r = round(l_n1 - l_n2, 3)
+            r = round((l_n1 - l_n2), 3)
             expression = str(r)
             return equation.set(r)
         if expression.find("+") > 0:
             r = expression.find("+")
             l_n1 = float(expression[0:r])
             l_n2 = float(expression[r+1:])
-            s = round(l_n1 + l_n2)
+            s = round((l_n1 + l_n2), 3)
             expression = str(s)
             return equation.set(s)
         if expression.find("/") > 0:
             r = expression.find("/")
             l_n1 = float(expression[0:r])
             l_n2 = float(expression[r+1:])
-            d = round(l_n1 / l_n2, 3)
+            d =  round((l_n1 / l_n2), 3)
             expression = str(d)
             return equation.set(d)
         if expression.find("*") > 0:
             r = expression.find("*")
             l_n1 = float(expression[0:r])
             l_n2 = float(expression[r+1:])
-            m = round(l_n1 * l_n2, 3)
+            m = round((l_n1 * l_n2), 3)
             expression = str(m)
             return equation.set(m)
     if num == "cl":
@@ -66,57 +70,57 @@ def press(num):
     # update the expression by using set method
     equation.set(expression)
 
+2
+n0 = tkinter.Button(text="0", width=2, height=2, highlightbackground=BLUE, command=lambda: press(0))
+n0.place(x=25, y=200)
 
-n0 = tkinter.Button(text="0", command=lambda: press(0))
-n0.place(x=35, y=210)
+n1 = tkinter.Button(text="1", width=2, height=2, highlightbackground=BLUE, command=lambda: press(1))
+n1.place(x=90, y=200)
 
-n1 = tkinter.Button(text="1", command=lambda: press(1))
-n1.place(x=100, y=210)
-
-n2 = tkinter.Button(text="2", command=lambda: press(2))
-n2.place(x=165, y=210)
+n2 = tkinter.Button(text="2", width=2, height=2,  highlightbackground=BLUE, command=lambda: press(2))
+n2.place(x=155, y=200)
 
 
-divide = tkinter.Button(text="/", command=lambda: press("/"))
-divide.place(x=230, y=210)
+divide = tkinter.Button(text="/", width=2, height=2, highlightbackground=BLUE, command=lambda: press("/"))
+divide.place(x=220, y=200)
 
-n3 = tkinter.Button(text="3", command=lambda: press(3))
-n3.place(x=35, y=275)
+n3 = tkinter.Button(text="3", width=2, height=2, highlightbackground=BLUE, command=lambda: press(3))
+n3.place(x=30, y=265)
 
-n4 = tkinter.Button(text="4", command=lambda: press(4))
-n4.place(x=100, y=275)
+n4 = tkinter.Button(text="4", width=2, height=2, highlightbackground=BLUE, command=lambda: press(4))
+n4.place(x=90, y=265)
 
-n5 = tkinter.Button(text="5", command=lambda: press(5))
-n5.place(x=165, y=275)
+n5 = tkinter.Button(text="5", width=2, height=2, highlightbackground=BLUE, command=lambda: press(5))
+n5.place(x=155, y=265)
 
-n6 = tkinter.Button(text="6", command=lambda: press(6))
-n6.place(x=165, y=275)
+n6 = tkinter.Button(text="6", width=2, height=2, highlightbackground=BLUE, command=lambda: press(6))
+n6.place(x=155, y=265)
 
-multiply = tkinter.Button(text="*", command=lambda: press("*"))
-multiply.place(x=225, y=275)
+multiply = tkinter.Button(text="*", width=2, height=2, highlightbackground=BLUE, command=lambda: press("*"))
+multiply.place(x=220, y=265)
 
-n7 = tkinter.Button(text="7", command=lambda: press(7))
-n7.place(x=35, y=335)
+n7 = tkinter.Button(text="7", width=2, height=2, highlightbackground=BLUE, command=lambda: press(7))
+n7.place(x=30, y=330)
 
-n8 = tkinter.Button(text="8", command=lambda: press(8))
-n8.place(x=100, y=335)
+n8 = tkinter.Button(text="8", width=2, height=2, highlightbackground=BLUE, command=lambda: press(8))
+n8.place(x=95, y=330)
 
-n9 = tkinter.Button(text="9", command=lambda: press(9))
-n9.place(x=165, y=335)
+n9 = tkinter.Button(text="9", width=2, height=2, highlightbackground=BLUE, command=lambda: press(9))
+n9.place(x=160, y=330)
 
-sum = tkinter.Button(text="+", command=lambda: press("+"))
-sum.place(x=225, y=335)
+sum = tkinter.Button(text="+", width=2, height=2, highlightbackground=BLUE, command=lambda: press("+"))
+sum.place(x=220, y=330)
 
-clear = tkinter.Button(text="cl", command=lambda: press("cl"))
-clear.place(x=35, y=400)
+clear = tkinter.Button(text="cl", width=2, height=2, highlightbackground=BLUE, command=lambda: press("cl"))
+clear.place(x=30, y=395)
 
-n5 = tkinter.Button(text="5", command=lambda: press("5"))
-n5.place(x=100, y=400)
+n5 = tkinter.Button(text="5", width=2, height=2, highlightbackground=BLUE, command=lambda: press("5"))
+n5.place(x=95, y=395)
 
-substra = tkinter.Button(text="-", command=lambda: press("-"))
-substra.place(x=165, y=400)
+substra = tkinter.Button(text="-", width=2, height=2, highlightbackground=BLUE, command=lambda: press("-"))
+substra.place(x=160, y=395)
 
-result = tkinter.Button(text="=", command=lambda: press("="))
-result.place(x=225, y=400)
+result = tkinter.Button(text="=", width=2, height=2, highlightbackground=BLUE, command=lambda: press("="))
+result.place(x=220, y=395)
 
 window.mainloop()
